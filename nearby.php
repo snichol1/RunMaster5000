6b9c20ed72824a8dc0a5315f66a551710f5a424e
Miguel Montemayor <miguel91@stanford.edu>
2012-10-31 23:32:17 -0700

    added nearby page. updated homepage links

---
+++ nearby.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search Results</title> 
+	<meta name="viewport" content="width=device-width, initial-scale=1"> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/	jquery.mobile-1.2.0.min.css" />
+	
+	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+		
+</head>
+
+<body>
+
+<!-- /header -->
+<div data-role="header">
+	<h1>Stanford Runs</h1>
+	<a href="home.php" data-icon="back">Home</a>
+</div>
+	
+<!--- Where all the main content goes! --->
+<div data-role="content">
+	
+<?php
+	include("config.php");
+
+	
+	$query = "SELECT * FROM Routes ORDER By Name";
+	
+	$result = mysql_query($query);
+	
+	while($row = mysql_fetch_assoc($result)){
+			echo "<div class='routeresult'><span class='nameresult'><a href=\"run.php?id=".$row["RouteID"]."\">".$row["Name"]."</a></span>";
+			echo "<span class='distanceresult'> Dist: ".$row["Distance"]."</span>";
+			echo "<span class='difficultyresult'> Diff: ".$row["Difficulty"]."</span></div>";
+	}
+	
+?>
+
+<div>
+
+</body>
+</html>
