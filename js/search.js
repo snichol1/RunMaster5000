ed5cd007ef539ea0af215a7a92323c25dc21d225
Miguel Montemayor <miguel91@stanford.edu>
2012-11-07 00:21:24 -0800

    search works better

---
+++ js/search.js
@@ -0,0 +1,9 @@
+function displayDistance(){
+	alert("worked");
+	console.log("hello");
+}
+
+$(".shortdist").change(function() {
+	alert("worked woo");
+	}
+);
--- searchresults.php
+++ searchresults.php
@@ -35,25 +35,31 @@
 	$med = $_GET['mediumdist'];
 	$long = $_GET['longdist'];
 	
+	$query = "";
+	$feedback = "";
+	
+	
 	//Checks to see what elements are selected.
 	if(!(empty($med) && empty($short) && empty($long))){
-		if(empty($med) && empty($short)){
-		    //echo("no med, short.");
-		    $minDist = 5;
-		}
-		else if(empty($short)){
-		    //echo("You didn't select short routes.");
-		    $minDist = 2;
-		}
-	
-		if(empty($med) && empty($long)){
-		    //echo("no med, long.");
-		    $maxDist = 2;
-		}
-		else if(empty($long)){
-		    //echo("You didn't select long routes.");
-		    $maxDist = 5;
-		}
+			if(empty($med) && empty($short)){
+			    //echo("no med, short.");
+			    $minDist = 5;
+			}
+			else if(empty($short)){
+			    //echo("You didn't select short routes.");
+			    $minDist = 2;
+			}
+		
+			if(empty($med) && empty($long)){
+			    //echo("no med, long.");
+			    $maxDist = 2;
+			}
+			else if(empty($long)){
+			    //echo("You didn't select long routes.");
+			    $maxDist = 5;
+			}
+				$query = "SELECT * FROM Routes WHERE Distance > ".$minDist." and Distance < ".$maxDist." and Difficulty > ".$minDiff." and Difficulty < ".$maxDiff;
+			
 		
 		$easy = $_GET['easydiff'];
 		$med2 = $_GET['mediumdiff'];
@@ -78,8 +84,20 @@
 		}
 	}
 
-	
-	$query = "SELECT * FROM Routes WHERE Distance > ".$minDist." and Distance < ".$maxDist." and Difficulty > ".$minDiff." and Difficulty < ".$maxDiff;
+<!-- Had to hardcode the random cases.. oops, but I really tried not to! (2 hours w/ nothing to show. Hardcoding took me 10 mins :P -->	
+
+if((empty($med) && !empty($short) && !empty($long)) && (empty($med2) && !empty($easy) && !empty($hard))){
+			$query = "SELECT * FROM Routes WHERE (Distance < 2 or Distance > 5) and Difficulty != 2";
+			$feedback = "Results for easy and hard routes that are <2 or >5 miles"
+		}
+		else if(empty($med) && !empty($short) && !empty($long)){
+			$query = "SELECT * FROM Routes WHERE (Distance < 2 or Distance > 5) and Difficulty > ".$minDiff." and Difficulty < ".$maxDiff;
+			$feedback .= " that are less than 2 miles or great than 5 miles"
+		} 
+		else if((empty($med2) && !empty($easy) && !empty($hard))){
+			$query = "SELECT * FROM Routes WHERE Distance > ".$minDist." and Distance < ".$maxDist." and Difficulty != 2";
+		}
+
 	
 	$result = mysql_query($query);
 	
@@ -91,7 +109,7 @@
 	
 	while($row = mysql_fetch_assoc($result)){
 		if($routeName === "" || (stristr($row["Name"], $_GET['name']) !== FALSE)){
-			echo "<div class='routeresult'><span class='nameresult'><a href=\"route.php?routeID=".$row["RouteID"]."\">".$row["Name"]."</a></span>";
+			echo "<div class='routeresult'><span class='nameresult'><a href=\"run.php?id=".$row["RouteID"]."\">".$row["Name"]."</a></span>";
 			echo "<span class='distanceresult'> Dist: ".$row["Distance"]."</span>";
 			echo "<span class='difficultyresult'> Diff: ".$row["Difficulty"]."</span></div>";
 		}
