c33b245119f6619961db4c7adc7d2dac0ac71392
Samuel McLaughlin <McFly@Samuels-MacBook-Air.local>
2012-11-20 15:14:36 -0800

    stopwatch fixed. derp.

--- run.php
+++ run.php
@@ -8,7 +8,7 @@
 	<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
 	<script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCv5woZWJa4qFr4nO4Dp9dCl3LrPQBMToE&sensor=false"></script>
-    <script src="js/runutilities.js"
+    <script src="js/runutilities.js"></script>
 
 </head> 
 <body> 
