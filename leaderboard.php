665593517e0881880187f8334d719cc98a367939
Miguel Montemayor <miguel91@stanford.edu>
2012-12-05 22:27:54 -0800

    leaderboard style major improvement

--- leaderboard.php
+++ leaderboard.php
@@ -26,7 +26,38 @@
 	<div data-role="content">	
 		
 		<div id="leaderboard">
+			<style>
+			.lheader{
+			font-size: 10px;
+			margin-top: 5px;
+			}
 			
+			.ui-grid-b{
+			margin-top: -0px;
+			}
+			
+			.headtitle{
+			font-size: 25px;
+			font-family: "Arial";
+			margin-bottom: 0px;
+			margin-left: auto;
+			margin-right: auto;
+			}
+			
+			.headtitlebottom{
+			font-size: 25px;
+			font-family: "Arial";
+			margin-bottom: 10px;
+			margin-top: 5px;
+			margin-left: auto;
+			margin-right: auto;	
+			}
+			
+			.routeresult{
+			font-size: 18px;
+			}
+			
+			</style>
 			<?php
 			include("config.php");
 			
@@ -42,29 +73,44 @@
 			$userID = $_GET['userID']; //$_SESSION['userID'];
 			$output = "It looks like no one has run this route. The leaderboard is empty.";
 			
+			
+			
+			
+
 			while($row = mysql_fetch_assoc($result)){
 							//echo "<div class='routeresult'><span class='userID'> ".$row["UserID"]."</span>";
 				$userquery = "SELECT * from Users where UserID=".$row["UserID"];
 				$result2 = mysql_query($userquery);
 				while($row2 = mysql_fetch_assoc($result2)){
 					if($output === "It looks like no one has run this route. The leaderboard is empty."){
-						$output = "<h3>All Leaderboard Results:</h3>";	
+						$output = "<div class='headtitlebottom'>All<div>";
+						$output = $output." <div class=\"ui-grid-b\">";
+					$output = $output." <div class='ui-block-a lheader'><b>RANK/NAME</b></div> <div class='ui-block-b lheader'><b>TIME</b></div> <div class='ui-block-c lheader'><b>DATE SET</b></div>";
 					}
 					$userName = $row2["Name"];
-					$output = $output." <div class='routeresult'><b>".$count.".</b> <span class='userName'>".$userName."</span> ";
+					$output = $output." <div class='routeresult username ui-block-a'><b>".$count.". </b> ".$userName."</div> ";
 					if($userID == $row["UserID"]){
-						echo "<div class='yourResult'><h3>Your Place in the Leaderboard:</h3>";
-						echo " <div class='routeresult'><b>".$count.".</b> <span class='userName'>".$userName."</span> ";	
-						echo " <span class='distanceresult'> <b>Time:</b> ".$row["min(Time)"]."</span> ";
-						echo " <span class='difficultyresult'> <b>Date:</b> ".$row["Date"]."</span></div> <br>";
+						if($count == 2)
+							echo "<div class='yourResult firstplace'>";
+						else
+							echo "<div class='yourResult'>";
+						echo "<div class='headtitle'>You</div>";
+						echo "<div class=\"ui-grid-b\">";
+						echo "<div class='ui-block-a lheader'><b>RANK/NAME</b></div> <div class='ui-block-b lheader'><b>TIME</b></div> <div class='ui-block-c lheader'><b>DATE SET</b></div>";
+						
+						echo " <div class='routeresult username ui-block-a'><b>".$count.". </b>".$userName."</div> ";	
+						echo " <div class='routeresult distanceresult ui-block-b'> <b></b> ".$row["min(Time)"]."</div> ";
+						echo " <div class='routeresult difficultyresult ui-block-c'> <b></b> ".$row["Date"]."</div> <br>";
+						echo "</div><br>";
 					}	
 				}
-				$output = $output." <span class='distanceresult'> <b>Time:</b> ".$row["min(Time)"]."</span> ";
-				$output = $output." <span class='difficultyresult'> <b>Date:</b> ".$row["Date"]."</span></div> ";
+				$output = $output." <div class='routeresult distanceresult ui-block-b'> <b></b> ".$row["min(Time)"]."</div> ";
+				$output = $output." <div class='routeresult difficultyresult ui-block-c'> <b></b> ".$row["Date"]."</div>";
 	
 				$count++;
 			}
 			echo $output;
+			echo "</div>";
 		?>
 		</div>
 		
